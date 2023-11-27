5479a52de2ccb2148cbc68881a78d82a8709c131
Amir <amirabudubai@gmail.com>
2023-11-26 23:06:23 -0600

    now logs instead of prints

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,16 +6,20 @@
 
 int main(int argc, char* argv[]){
 	
-
+	openlog(NULL,0,LOG_USER);
+	
 	if(argc < 2){
-		printf("Not enough arguments \n");
+		syslog(LOG_ERR,"Not enough arguments \n");
 		return 1; 
 	}
+
+
+	syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]); 
 	
 	FILE* f = fopen(argv[1],"w");
 	if(f == NULL){
-		int e_code = errno; 
-		printf("Error opening %s: %s \n",argv[1],strerror(e_code)); 
+		//int e_code = errno; 
+		syslog(LOG_ERR,"Error opening %s: %s \n",argv[1],strerror(errno)); 
 		return 1; 
 	}
 
