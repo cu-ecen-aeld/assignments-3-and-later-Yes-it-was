6633fb835e214785a9e8ebecc74a28f00e682637
Amir <amirabudubai@gmail.com>
2023-12-03 13:39:03 -0600

    do_system()

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+	int code = system(cmd); 
+	if(code != 0)
+		return false; 
+	return true;
 }
 
 /**
