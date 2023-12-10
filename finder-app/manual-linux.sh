24be643e7abe3ec44269a703fea267f652aa5350
Amir <amirabudubai@gmail.com>
2023-12-09 21:03:15 -0600

    booting, but not copying scripts

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -53,7 +53,9 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 
 fi
 
+#was this suppose to be a TODO?
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -64,10 +66,12 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
+
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
-
 
 
 cd "$OUTDIR"
@@ -90,9 +94,9 @@ make defconfig
 #Build
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 #create symlinks 
-make CONFIG_PREFIX=${OUTDIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 #go back to root
-cd ${OUTDIR}
+cd ${OUTDIR}/rootfs
 
 
 echo "Library dependencies"
@@ -102,20 +106,35 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # TODO: Add library dependencies to rootfs
 LIBC=$(aarch64-none-linux-gnu-gcc -print-sysroot -v)
 #echo The value of libc is $LIBC
-cp $LIBC/lib64/libm.so.6 lib64/libm.so.6
-cp $LIBC/lib64/libresolv.so.2 lib64/libresolv.so.2
-cp $LIBC/lib64/libc.so.6 lib64/libc.so.6
+cp $LIBC/lib64/libm.so.6 $OUTDIR/rootfs/lib64/libm.so.6
+cp $LIBC/lib64/libresolv.so.2 $OUTDIR/rootfs/lib64/libresolv.so.2
+cp $LIBC/lib64/libc.so.6 $OUTDIR/rootfs/lib64/libc.so.6
 
-cp $LIBC/lib/ld-linux-aarch64.so.1 lib/ld-linux-aarch64.so.1
+cp $LIBC/lib/ld-linux-aarch64.so.1 $OUTDIR/rootfs/lib/ld-linux-aarch64.so.1
 
 
 # TODO: Make device nodes
+cd $OUTDIR/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
+
 
 # TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make clean 
+make CROSS_COMPILE=$CROSS_COMPILE writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp build/writer $OUTDIR/rootfs/home
+
 
 # TODO: Chown the root directory
+cd $OUTDIR/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd $OUTDIR
+gzip -f initramfs.cpio
