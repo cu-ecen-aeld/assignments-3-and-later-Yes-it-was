80f832756c79ab9021179344ec922a50f69746a8
Amir <amirabudubai@gmail.com>
2023-12-16 22:18:40 -0600

    added output for assigment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -60,6 +60,9 @@ do
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+#assignment 4 output file
+echo ${OUTPUTSTRING} >> /tmp/assignment4-result.txt
+
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
